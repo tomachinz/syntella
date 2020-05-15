d5ff6a09288a1bce82827201684170978e25b404
Tom Atkinson <tom@funk.co.nz>
2020-05-16 00:35:32 +1200

    Massively faster, long running tasks are run in background now

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ The report contains the following sensitive information:
 Wifi names, Mac addresses, processes, programs, ports, servers, services, disk free, network connections, open files.
 
 We don't capture any user files or filenames, unless they are open. So consider which websites you were visited just before running it. Netstat will still show closed connections for at least 2 minutes after closing, due to the the CLOSEWAIT and TIMEWAIT states of TCP/IP.
+
+## installation
+Drag the app into your Utilities or Applications folders
+
+Double click the 04B Font, and click istall - to make it look cooler when it runs. 
--- syntella.sh
+++ syntella.sh
@@ -1,9 +1,10 @@
 HOST=$(hostname)
 TIMESTAMP=$(date +%s)
+FILEPATH="$HOME/syntella/"
 LOGFILE="syntella-$HOST-$TIMESTAMP.txt"
+echo Log file path: $FILEPATH
 touch $LOGFILE
-
-
+rm tmp_syntella_file_*
 
 echo >> $LOGFILE
 echo >> $LOGFILE
@@ -13,114 +14,148 @@ echo " \_____  <   |  |/  ___/\   __\/ __ \ /     \|   |/    \   __\/ __ \|  |  
 echo " /        \___  |\___ \  |  | \  ___/|  Y Y  \   |   |  \  | \  ___/|  |__"     >>  $LOGFILE
 echo "/_______  / ____/____  > |__|  \___  >__|_|  /___|___|  /__|  \___  >____/"     >>  $LOGFILE
 echo "        \/\/         \/            \/      \/         \/          \/      "     >>  $LOGFILE
-echo "==============v1.7========================================================"     >>  $LOGFILE
+echo "==============v0.2.0========================================================"     >>  $LOGFILE
 echo >> $LOGFILE
 echo >> $LOGFILE
-
-
 
 tell_user () {
- echo "                                         =///"
- echo "-------------------------------------------"
- echo $1
- echo "___________________________________________"
+ echo "                                                       =///"  >>  $LOGFILE
+ echo "___________________________________________________________" >>  $LOGFILE
+ echo                 $1                                            >>  $LOGFILE
+ echo "___________________________________________________________" >>  $LOGFILE
 }
 
-tell_user "System Intel Tella by Tom Atkinson"
-echo "Your report filename is: $LOGFILE ";
+backgrounder () {
+  NOSPACES=$(echo $1 | sed "s/ /_/g")
+  BACKGROUNDFILE="tmp_syntella_file_$NOSPACES.txt"
+  echo  "Section: $1    - $2 "   >> $BACKGROUNDFILE
+  eval $2 >> $BACKGROUNDFILE
+  echo                  >>  $BACKGROUNDFILE
+  uptime >> $BACKGROUNDFILE
+  echo "___________________________________________________________" >>  $BACKGROUNDFILE
+  echo                  >>  $BACKGROUNDFILE
+  # Dump the contents in one go to allow this function to run in parallel
+  cat $BACKGROUNDFILE >> $LOGFILE
+  rm $BACKGROUNDFILE
+}
+check_pings () {
+  tell_user "Checking pings times to USA, NZ, Australia, Russia, UK"
+  echo background file: $BACKGROUNDFILE
+  ping -c 3 8.8.8.8                                            >>  $BACKGROUNDFILE
+  # ping -c 2 akl.funk.nz                                        >>  $BACKGROUNDFILE
+  ping -c 3 sydney.funk.nz                                     >>  $BACKGROUNDFILE
+  ping -c 3 funk.nz                                            >>  $BACKGROUNDFILE
+  ping -c 3 www.rt.com                                         >>  $BACKGROUNDFILE
+  ping -c 3 www.parliament.uk                                  >>  $BACKGROUNDFILE
+}
+
+tell_user "System Intel Tella by Tom Atkinson - syntella@tomachi.co"
+echo "Your report filename is: $LOGFILE";
 echo "Presently your report is located in the Application bundle"
 echo "When it's complete it will be copied to ~/syntella/$LOGFILE"
 echo "and uploaded to Tomachi Corporation in New Zealand"
-echo "syntella@tomachi.co"
 echo "Your use of the program implies tacit consent for Tomachi Corporation to"
 echo "use in future to develop network anti-virus security software for good."
-echo "Your URL is based on the filename and will be displayed at the end"
-echo "On my machine it takes 6 minutes to run... "
-
 echo
+
+
+# tell_user "Running Apple system profiler"
+# system_profiler  SPParallelATADataType   >>  $LOGFILE
+# system_profiler  SPUniversalAccessDataType   >>  $LOGFILE
+# system_profiler  SPApplicationsDataType   >>  $LOGFILE
+backgrounder "SPApplicationsDataType" 'system_profiler SPApplicationsDataType | grep Location' &
+# system_profiler  SPAudioDataType   >>  $LOGFILE
+# system_profiler  SPBluetoothDataType   >>  $LOGFILE
+# system_profiler  SPCameraDataType   >>  $LOGFILE
+# system_profiler  SPCardReaderDataType   >>  $LOGFILE
+# system_profiler  SPComponentDataType   >>  $LOGFILE
+# system_profiler  SPiBridgeDataType   >>  $LOGFILE
+# system_profiler  SPDeveloperToolsDataType   >>  $LOGFILE
+backgrounder "Developer Tools" 'system_profiler SPDeveloperToolsDataType' &
+backgrounder "Disabled Software" 'system_profiler SPDisabledSoftwareDataType' &
+backgrounder "Extensions" 'system_profiler SPExtensionsDataType' &
+backgrounder "Firewall Setup" 'system_profiler SPFirewallDataType' &
+# system_profiler  SPDiagnosticsDataType   >>  $LOGFILE
+# system_profiler  SPDiscBurningDataType   >>  $LOGFILE
+# system_profiler  SPEthernetDataType   >>  $LOGFILE
+# system_profiler  SPFibreChannelDataType   >>  $LOGFILE
+# system_profiler  SPFireWireDataType   >>  $LOGFILE
+# system_profiler  SPFontsDataType   >>  $LOGFILE
+# system_profiler  SPFrameworksDataType   >>  $LOGFILE
+# system_profiler  SPDisplaysDataType   >>  $LOGFILE
+# system_profiler  SPHardwareDataType   >>  $LOGFILE
+# system_profiler  SPHardwareRAIDDataType   >>  $LOGFILE
+backgrounder "Installers History" 'system_profiler SPInstallHistoryDataType' &
+backgrounder "Network Location" 'system_profiler SPNetworkLocationDataType' &
+# system_profiler  SPLogsDataType   >>  $LOGFILE
+# system_profiler  SPManagedClientDataType   >>  $LOGFILE
+# system_profiler  SPMemoryDataType   >>  $LOGFILE
+# system_profiler  SPNVMeDataType   >>  $LOGFILE
+backgrounder "Network" 'system_profiler SPNetworkDataType' &
+# system_profiler  SPPCIDataType   >>  $LOGFILE
+# system_profiler  SPParallelSCSIDataType   >>  $LOGFILE
+# system_profiler  SPPowerDataType   >>  $LOGFILE
+# system_profiler  SPPrefPaneDataType   >>  $LOGFILE
+# system_profiler  SPPrintersSoftwareDataType   >>  $LOGFILE
+# system_profiler  SPPrintersDataType   >>  $LOGFILE
+backgrounder "Configuration" 'system_profiler SPConfigurationProfileDataType' &
+# system_profiler SPRawCameraDataType   >>  $LOGFILE
+# system_profiler  SPSASDataType   >>  $LOGFILE
+# system_profiler  SPSerialATADataType   >>  $LOGFILE
+# system_profiler  SPSPIDataType   >>  $LOGFILE
+# system_profiler  SPSmartCardsDataType   >>  $LOGFILE
+backgrounder "SPSoftwareDataType" 'system_profiler SPSoftwareDataType' &
+backgrounder "SPStartupItemDataType" 'system_profiler SPStartupItemDataType' &
+# system_profiler  SPStorageDataType   >>  $LOGFILE
+# system_profiler  SPSyncServicesDataType   >>  $LOGFILE
+# system_profiler  SPThunderboltDataType   >>  $LOGFILE
+# system_profiler  SPUSBDataType   >>  $LOGFILE
+# system_profiler  SPNetworkVolumeDataType   >>  $LOGFILE
+# system_profiler  SPWWANDataType   >>  $LOGFILE
+# system_profiler  SPAirPortDataType >>  $LOGFILE
+backgrounder "Netstat" 'netstat -Walt | grep -v -E "kctl|kevt|dgram|kevt|stream" | grep -E "tcp|udp|icmp"' &
+backgrounder "List of open files with internet servers" 'lsof -Pnl +M -i -cmd | grep -E "LISTEN|TCP|UDP"' &
+backgrounder "Check ping times" 'check_pings' &
+backgrounder "Networok Map" 'nmap -sV -T4 -O -F --version-light localhost' &
 
 tail -f $LOGFILE  &
 hostname                                                     >>  $LOGFILE
 uname -a                                                     >>  $LOGFILE
 tell_user "The following users are logged in"
 w                                                            >>  $LOGFILE
-tell_user "Logging current network sockets (uno momento)"
-netstat -Walt                                                >>  $LOGFILE
-tell_user "Checking router tables (uno momento)"
 
-netstat -Wr                                                  >>  $LOGFILE
-tell_user "nmap -A localhost | Shows listening servers on local"
-nmap -A localhost                                            >>  $LOGFILE
-tell_user "Running lsof | Process names of servers"
-lsof -Pnl +M -i -cmd | grep -E "LISTEN|TCP|UDP"              >>  $LOGFILE
-tell_user "Logging running processes"
+tell_user "Logging current network sockets with netstat (uno momento)"
+echo "Active Internet connections (including servers)"       >>  $LOGFILE
+echo "Proto Recv-Q Send-Q  Local Address          Foreign Address        (state)" >>  $LOGFILE
+
+tell_user "Checking router tables (uno momento)"
+netstat -Wrn                                                  >>  $LOGFILE
+
+tell_user "Running processes"
 ps -ax                                                       >>  $LOGFILE
 echo "$(ps ax | wc -l) running processes detected by syntella"
-df -h
-tell_user "Checking pings times to USA, NZ, Australia, Russia, UK"
-ping -c 2 8.8.8.8                                            >>  $LOGFILE
-ping -c 2 akl.funk.nz                                        >>  $LOGFILE
-ping -c 2 sydney.funk.nz                                     >>  $LOGFILE
-ping -c 2 funk.nz                                            >>  $LOGFILE
-ping -c 2 www.rt.com                                         >>  $LOGFILE
-ping -c 2 www.parliament.uk                                  >>  $LOGFILE
 
-tell_user "Running Apple system profiler"
-system_profiler  SPParallelATADataType   >>  $LOGFILE
-# system_profiler  SPUniversalAccessDataType   >>  $LOGFILE
-system_profiler  SPApplicationsDataType   >>  $LOGFILE
-system_profiler  SPAudioDataType   >>  $LOGFILE
-system_profiler  SPBluetoothDataType   >>  $LOGFILE
-system_profiler  SPCameraDataType   >>  $LOGFILE
-system_profiler  SPCardReaderDataType   >>  $LOGFILE
-system_profiler  SPComponentDataType   >>  $LOGFILE
-system_profiler  SPiBridgeDataType   >>  $LOGFILE
-system_profiler  SPDeveloperToolsDataType   >>  $LOGFILE
-system_profiler  SPDiagnosticsDataType   >>  $LOGFILE
-system_profiler  SPDisabledSoftwareDataType   >>  $LOGFILE
-system_profiler  SPDiscBurningDataType   >>  $LOGFILE
-system_profiler  SPEthernetDataType   >>  $LOGFILE
-system_profiler  SPExtensionsDataType   >>  $LOGFILE
-system_profiler  SPFibreChannelDataType   >>  $LOGFILE
-system_profiler  SPFireWireDataType   >>  $LOGFILE
-system_profiler  SPFirewallDataType   >>  $LOGFILE
-system_profiler  SPFontsDataType   >>  $LOGFILE
-system_profiler  SPFrameworksDataType   >>  $LOGFILE
-system_profiler  SPDisplaysDataType   >>  $LOGFILE
-system_profiler  SPHardwareDataType   >>  $LOGFILE
-system_profiler  SPHardwareRAIDDataType   >>  $LOGFILE
-system_profiler  SPInstallHistoryDataType   >>  $LOGFILE
-system_profiler  SPNetworkLocationDataType   >>  $LOGFILE
-system_profiler  SPLogsDataType   >>  $LOGFILE
-system_profiler  SPManagedClientDataType   >>  $LOGFILE
-system_profiler  SPMemoryDataType   >>  $LOGFILE
-system_profiler  SPNVMeDataType   >>  $LOGFILE
-system_profiler  SPNetworkDataType   >>  $LOGFILE
-system_profiler  SPPCIDataType   >>  $LOGFILE
-system_profiler  SPParallelSCSIDataType   >>  $LOGFILE
-system_profiler  SPPowerDataType   >>  $LOGFILE
-system_profiler  SPPrefPaneDataType   >>  $LOGFILE
-system_profiler  SPPrintersSoftwareDataType   >>  $LOGFILE
-system_profiler  SPPrintersDataType   >>  $LOGFILE
-system_profiler  SPConfigurationProfileDataType
-# system_profiler SPRawCameraDataType   >>  $LOGFILE
-system_profiler  SPSASDataType   >>  $LOGFILE
-system_profiler  SPSerialATADataType   >>  $LOGFILE
-system_profiler  SPSPIDataType   >>  $LOGFILE
-system_profiler  SPSmartCardsDataType   >>  $LOGFILE
-system_profiler  SPSoftwareDataType   >>  $LOGFILE
-system_profiler  SPStartupItemDataType   >>  $LOGFILE
-system_profiler  SPStorageDataType   >>  $LOGFILE
-system_profiler  SPSyncServicesDataType   >>  $LOGFILE
-system_profiler  SPThunderboltDataType   >>  $LOGFILE
-system_profiler  SPUSBDataType   >>  $LOGFILE
-system_profiler  SPNetworkVolumeDataType   >>  $LOGFILE
-system_profiler  SPWWANDataType   >>  $LOGFILE
-system_profiler  SPAirPortDataType >>  $LOGFILE
+df -h      >>  $LOGFILE
 
-tell_user "https upload to tomachi.co"
-w                                                            >>  $LOGFILE
-echo "Finito.Finito.Finito."                                 >>  $LOGFILE
+tell_user "Checking domain name resolvers with scutil --dns"
+scutil --dns                                                 >>  $LOGFILE
+
+w  >>  $LOGFILE
+
+tell_user "Last 24 logins..."
+last | tail -n 24 >> $LOGFILE
+
+TOGO=`expr $(ls -lah | grep tmp_syntella_file | wc -l)`
+while [ $TOGO -gt 0 ]; do
+  echo waiting for $TOGO to finish
+  sleep $TOGO
+  sleep 2
+  TOGO=`expr $(ls -lah | grep tmp_syntella_file | wc -l)`
+done
+URL="https://tomachi.co/syntella-uploads/uploads/$LOGFILE";
+uptime                                         >>  $LOGFILE
+echo "Finito."                                 >>  $LOGFILE
+
 echo >> $LOGFILE
 echo >> $LOGFILE
 echo "  _________               __                 .___        __         .__   "     >>  $LOGFILE
@@ -132,18 +167,20 @@ echo "        \/\/         \/            \/      \/         \/          \/      
 echo "=========================================================================="     >>  $LOGFILE
 echo >> $LOGFILE
 echo >> $LOGFILE
-echo "Your report filename is: $LOGFILE " >>  $LOGFILE
-echo "https://tomachi.co/syntella-uploads/uploads/$LOGFILE " >>  $LOGFILE
-sleep 1
-curl -v -X POST -F "synreport=@$LOGFILE" https://tomachi.co/syntella-uploads/fileUpload.php
+echo "This report URL is: $URL " >>  $LOGFILE
+echo
+echo
+mkdir -p $FILEPATH
+cp $LOGFILE $FILEPATH
+echo "Begin https upload to Tomachi Corporation"
 
-sleep 1
-
-open $LOGFILE
-
-mkdir ~/syntella
-cp $LOGFILE ~/syntella
-rm $LOGFILE
-echo "Complete. Your report filename is located at: ~/syntella/$LOGFILE "
-echo "https://tomachi.co/syntella-uploads/uploads/$LOGFILE ";
+curl --progress-bar --referer https://tomachi.co -F "synreport=@$LOGFILE" https://tomachi.co/syntella-uploads/fileUpload.php
+echo
+echo "Complete. Your report filename is located at: ~/syntella/$LOGFILE"
+echo $URL
+open "https://tomachi.co/syntella-uploads/uploads/$LOGFILE"
+# open $LOGFILE
 sleep 2
+rm $LOGFILE
+rm tmp_syntella_file_* 2>/dev/null
+rm syntella-*-*.txt 2>/dev/null
